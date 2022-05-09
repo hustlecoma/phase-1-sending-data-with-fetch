221c6cd45a6744605c148b1a0f95c34c5e53be09
Frank Lee <plat4m@outlook.com>
2022-05-08 19:49:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+function submitData(name, email) {
+
+    const configurationObject = {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        }),
+    
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+
+    .then((response) => response.json())
+    
+    
+    .then((object) => {
+            document.body.append(object.id);
+        })
+    
+    
+    .catch((error) => {
+            alert("Unauthorized Access");
+            document.body.append(error.message);
+        });
+}
+
+submitData();
